4bc1c219e5fecea9e012d817fd95ce75ebc7b304
yijie <ngyijie92@gmail.com>
2022-04-08 14:39:37 +0800

    Edit dashboard and login

--- server/index.js
+++ server/index.js
@@ -1,19 +1,30 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const session = require('express-session');
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 3001;
 
+app.use(cors());
+
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true})); // Setup the body parser to handle form submits
 
+// Generate a unique session id, store session id in a session cookie, create empty session object
 app.use(session({
     secret: 'secret',
 	resave: true,
 	saveUninitialized: true
-})); // Generate a unique session id, store session id in a session cookie, create empty session object
+})); 
 
-const db = require('../database');
+const db = require('./database');
+
+// Routes
+app.post('/signup', (req, res) => {
+    db.query("INSERT INTO accounts (username, password, email) VALUES (?,?)", [username, password, email], (err, res) => {
+        console.log(err);
+    });
+});
 
 app.post('/login', (req, res) => {
     const username = req.body.username;
@@ -27,7 +38,7 @@ app.post('/login', (req, res) => {
                 res.send({err: err})
             }
 
-            if (result) {
+            if (result.length > 0) {
                 res.send(result);
             } else {
                 res.send({ message: "Wrong username/password combination!" });
